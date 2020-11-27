0c6a758fb1b97e017210f403411c2626c47d4a6c
Aleks <aadamovic@attentivemobile.com>
2020-11-27 12:43:03 -0500

    feat: initial order-service

---
+++ order-service/src/test/java/com/springbootplayground/order/service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootplayground.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
